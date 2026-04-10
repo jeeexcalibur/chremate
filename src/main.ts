16ba92ea8aed4006120f630e777f0853cedc65bf
Jein Ananda <112226147+jeeexcalibur@users.noreply.github.com>
2026-04-11 00:48:49 +0800

    feat: vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/index.css'
+import { Analytics } from "@vercel/analytics/vue"
 
 const app = createApp(App)
 
